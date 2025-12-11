e9ee984f170595f855078513251c96cfedc75c8e
Clatan <149095889+Clatan@users.noreply.github.com>
2025-12-11 16:17:58 +0700

    signup-signin

---
+++ app/src/main/java/com/example/mobileprogramming_project/SignUp.java
@@ -0,0 +1,49 @@
+package com.example.mobileprogramming_project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView tvBirthday = findViewById(R.id.tvBirthday);
+        ImageView btnCalendar = findViewById(R.id.btnCalendar);
+
+        View.OnClickListener openCalendar = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SignUp.this,
+                        (view, y, m, d) -> tvBirthday.setText(String.format("%02d / %02d / %04d", d, m+1, y)),
+                        year, month, day);
+
+                dialog.show();
+            }
+        };
+
+        tvBirthday.setOnClickListener(openCalendar);
+
+        btnCalendar.setOnClickListener(openCalendar);
+
+    }
+}
