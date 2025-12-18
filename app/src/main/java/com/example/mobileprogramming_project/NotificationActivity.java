a74ff9c119730e0e7bd3920bf1f7fd4ffb3ee16c
j <j3sslyna@gmail.com>
2025-12-19 01:57:17 +0700

    added placeholders for notification and profile, and edited the id in the xml

--- app/src/main/java/com/example/mobileprogramming_project/NotificationActivity.java
+++ app/src/main/java/com/example/mobileprogramming_project/NotificationActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileprogramming_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,27 @@ public class NotificationActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        // buat interactive btns
+        ImageView btnBack = findViewById(R.id.btnBack);
+        ImageView btnProfile = findViewById(R.id.btnProfile);
+        ImageView btnSettings = findViewById(R.id.btnSettings);
+
+        //buat click, kl di klik btn backnya bakal tutup activitynya
+        btnBack.setOnClickListener(v ->finish());
+
+        //buat button profile, bakal pindah page ke profile class
+        btnProfile.setOnClickListener(v ->
+                startActivity(new Intent(this, ProfileActivity.class))
+        );
+
+        //buat settings hrsnya sm kek button, cm krn blm ad pake toast dl
+        btnSettings.setOnClickListener(v->
+                Toast.makeText(this, "settings page placeholder", Toast.LENGTH_SHORT).show()
+        );
+
+
     }
+
+
 }
--- app/src/main/java/com/example/mobileprogramming_project/ProfileActivity.java
+++ app/src/main/java/com/example/mobileprogramming_project/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileprogramming_project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,31 @@ public class ProfileActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button btnEditProfile = findViewById(R.id.btnEditProfile);
+        TextView tvMenuSettings = findViewById(R.id.tvMenuSettings);
+        TextView tvMenuAboutUs = findViewById(R.id.tvMenuAboutUs);
+        TextView tvMenuHelp = findViewById(R.id.tvMenuHelp);
+
+        //placeholder dulu
+        btnEditProfile.setOnClickListener(v ->
+                Toast.makeText(this, "edit profile placeholder", Toast.LENGTH_SHORT).show()
+        );
+
+        //placeholder dulu
+        tvMenuSettings.setOnClickListener(v ->
+                Toast.makeText(this, "settings placeholder", Toast.LENGTH_SHORT).show()
+        );
+
+        //placeholder dulu
+        tvMenuAboutUs.setOnClickListener(v ->
+                Toast.makeText(this, "about us placeholder", Toast.LENGTH_SHORT).show()
+        );
+
+        //placeholder dulu
+        tvMenuHelp.setOnClickListener(v ->
+                Toast.makeText(this, "help placeholder", Toast.LENGTH_SHORT).show()
+        );
+
     }
 }
